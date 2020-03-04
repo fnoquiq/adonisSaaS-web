f90626977177cbfa96c81192bf786ef7f54d2393
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-03-04 19:51:08 -0300

    :rocket: Adiciona Selecionador de projetos

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,39 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Button from '~/styles/components/Button';
 
 import { Container, Project } from './styles';
 
-const Projects = () => (
-  <Container>
-    <header>
-      <h1>Rocketseat</h1>
-      <div>
-        <Button onClick={() => {}}>+ Novo</Button>
-        <Button onClick={() => {}}>Membros</Button>
-      </div>
-    </header>
+const Projects = ({ teamActive }) => {
+  if (!teamActive) return null;
 
-    <Project>
-      <p>Projeto Teste 1</p>
-    </Project>
+  return (
+    <Container>
+      <header>
+        <h1>{teamActive.name}</h1>
+        <div>
+          <Button onClick={() => {}}>+ Novo</Button>
+          <Button onClick={() => {}}>Membros</Button>
+        </div>
+      </header>
 
-    <Project>
-      <p>Projeto Teste 2</p>
-    </Project>
+      <Project>
+        <p>Projeto Teste 1</p>
+      </Project>
 
-    <Project>
-      <p>Projeto Teste 3</p>
-    </Project>
+      <Project>
+        <p>Projeto Teste 2</p>
+      </Project>
 
-    <Project>
-      <p>Projeto Teste 4</p>
-    </Project>
+      <Project>
+        <p>Projeto Teste 3</p>
+      </Project>
 
-    <Project>
-      <p>Projeto Teste 5</p>
-    </Project>
-  </Container>
-);
+      <Project>
+        <p>Projeto Teste 4</p>
+      </Project>
 
-export default Projects;
+      <Project>
+        <p>Projeto Teste 5</p>
+      </Project>
+    </Container>
+  );
+};
+
+Projects.propTypes = {
+  activeTeam: PropTypes.shape({
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+const mapStateToProps = state => ({
+  teamActive: state.teams.active,
+});
+
+export default connect(mapStateToProps)(Projects);
